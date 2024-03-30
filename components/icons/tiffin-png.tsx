b88424ee37c9d3bd0c6dd5b6124e382f484c4286
Virtual Educator <152733764+Virtual-Educator@users.noreply.github.com>
2024-03-30 15:03:23 -0400

    Update tiffin-png.tsx

--- components/icons/tiffin-png.tsx
+++ components/icons/tiffin-png.tsx
@@ -2,26 +2,22 @@ import { FC } from "react";
 
 interface TiffinUIPNGProps {
   src: string;
-  height?: number;
-  width?: number;
   className?: string;
   alt?: string;
 }
 
 export const TiffinUIPNG: FC<TiffinUIPNGProps> = ({
   src,
-  height = 40,
-  width = 40,
   className,
-  alt = ""
+  alt = "Tiffin University Chatbot UI"
 }) => {
   return (
     <img
       className={className}
-      src= https://imgur.com/a/UpeaZrC
-      height= 256
-      width= 256
-      alt= Tiffin University Chatbot UI
+      src={src}
+      height="256" // If height is always 256
+      width="256" // If width is always 256
+      alt={alt}
     />
   );
 };
